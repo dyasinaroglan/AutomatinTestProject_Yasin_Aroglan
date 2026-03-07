2a84cb5f7898ff207ebc76470a7979b51979cabe
AR009 <yasinargln264@gmail.com>
2026-03-07 23:58:35 +0300

    pom.xml dosyasını Selenium, TestNG ve WebDriverManager bağımlılıkları eklendi.
    Sayfa Nesne Modeli (Page Object Model) desenini izleyerek proje
    yapısı oluşturulmaya başlandı.
    
    Yeniden kullanılabilir bekleme,tıklama ve doğrulama yöntemleriyle BasePage'e eklendi.

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -1,4 +1,63 @@
 package pages;
 
-public class BasePage {
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public abstract class BasePage {
+
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+    protected Actions actions;
+
+    private static final Duration defaultWait = Duration.ofSeconds(10);
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, defaultWait);
+        this.actions = new Actions(driver);
+        PageFactory.initElements(driver, this);
+    }
+
+    protected WebElement waitForVisibility(By locator) {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    protected WebElement waitForClickability(By locator) {
+        return wait.until(ExpectedConditions.elementToBeClickable(locator));
+    }
+
+    protected boolean waitForUrl(String urlFragment) {
+        return wait.until(ExpectedConditions.urlContains(urlFragment));
+    }
+
+    protected void click(By locator) {
+        waitForClickability(locator);
+    }
+
+    protected String getText(By locator) {
+        return waitForVisibility(locator).getText().trim();
+    }
+
+    protected boolean isElementDisplayed(By locator) {
+        try {
+            return waitForVisibility(locator).isDisplayed();
+        } catch (TimeoutException e) {
+            return false;
+        }
+    }
+    protected String getCurrentUrl(){
+        return driver.getCurrentUrl();
+    }
+    protected String getPageTitle(){
+        return driver.getTitle();
+    }
+    protected void clickWithJS(By locator) {
+        WebElement element = waitForVisibility(locator);
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", element);
+    }
 }
